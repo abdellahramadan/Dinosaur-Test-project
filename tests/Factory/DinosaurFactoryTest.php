4e70104d70e8ae94d6ee9bfb9a53535ff82bc19a
giantthinker <ramadanabdel24@gmail.com>
2021-07-07 13:52:23 +0200

    different test hooks

--- tests/Factory/DinosaurFactoryTest.php
+++ tests/Factory/DinosaurFactoryTest.php
@@ -8,14 +8,40 @@ use App\Entity\Dinosaur;
 
 class DinosaurFactoryTest extends TestCase
 {
+    /**
+     * @var DinosaurFactory
+     */
+    private $factory; 
+
+    public function setUp(): void
+    {
+        $this->factory = new DinosaurFactory();
+    }
+
     public function testItGrowsAVelociraptor()
     { 
-        $factory = new DinosaurFactory();
-        $dinosaur = $factory->growVelociraptor(5);
+        $dinosaur = $this->factory->growVelociraptor(5);
 
         $this->assertInstanceOf(Dinosaur::class, $dinosaur);
         // $this->assertInternalType('string', $dinosaur->getGenus()); Deprecated assertion
         $this->assertSame('Velociraptor', $dinosaur->getGenus());
         $this->assertSame(5, $dinosaur->getLength());
+    }
+
+    // incomplete test
+    public function testItGrowsATriceraptops()
+    { 
+        $this->markTestIncomplete('Waiting for confirmation from GenLab hahahaha...');
+    }
+
+    //Skipping a test
+    public function testItGrowsABabyVelociraptor()
+    { 
+        if (!class_exists('Nanny')) { 
+            $this->markTestSkipped('There is nobody to watch the baby');
+        }
+
+        $dinosaur = $this->factory->growVelociraptor(1);
+        $this->assertSame(1, $dinosaur->getLength());
     }
 }
